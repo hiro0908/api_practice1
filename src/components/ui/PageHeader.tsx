122dda0aed94d8c212d4108eba97262f311eb183
hiro0908 <hiro54924@gmail.com>
2026-07-16 17:12:49 +0900

    検索バーの初期値の設定

--- src/components/ui/PageHeader.tsx
+++ src/components/ui/PageHeader.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { Settings } from "lucide-react";
 
 export function PageHeader() {
-  const [inputNumber, setInputNumber] = useState("0");
+  const [inputNumber, setInputNumber] = useState("");
   const router = useRouter();
 
   return (
