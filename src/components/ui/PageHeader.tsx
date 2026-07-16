4ae1f004f26e1e18d1b8c84d8af68f09d2603b7b
hiro0908 <hiro54924@gmail.com>
2026-07-16 15:58:39 +0900

    設定ボタンと検索バーの隙間問題の改善

--- src/components/ui/PageHeader.tsx
+++ src/components/ui/PageHeader.tsx
@@ -19,8 +19,8 @@ export function PageHeader() {
       >
         <PageTitle title="ポケモン図鑑" />
       </button>
-      <div className="flex flex-row items-end justify-end">
-        <div className="shadow-md rounded-full gap-3">
+      <div className="flex flex-row items-end  gap-7 justify-end">
+        <div className="shadow-md rounded-full">
           <ButtonGroupInput value={inputNumber} onChange={setInputNumber} />
         </div>
         <Link
